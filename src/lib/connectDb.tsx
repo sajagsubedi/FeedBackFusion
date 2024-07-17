2d40dfa543938952b783cf551df9210f0fd0f7d5
Sajag Subedi <108583402+sajagsubedi@users.noreply.github.com>
2024-07-17 11:07:24 +0000

    Db connection added

---
+++ src/lib/connectDb.tsx
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+
+const connection: connectionObject = {};
+
+const connectDb = async () => {
+  if (connection.isConnected) {
+    return;
+  }
+  try {
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+      throw new Error("MONGO_URI environment variable is not defined.");
+    }
+    const db = await mongoose.connect(uri);
+    connection.isConnected = db.connections[0].readyState;
+  } catch (error) {
+    process.exit(1);
+  }
+};
+
+export default connectDb;
