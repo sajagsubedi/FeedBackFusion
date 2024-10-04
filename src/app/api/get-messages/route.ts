0fac3b018c73ba2c003b61deb07d21fcd2781214
Sajag Subedi <sajagsubedi03@gmail.com>
2024-10-04 17:16:11 +0545

    get messages route added

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,59 @@
+import connectDb from "@/lib/connectDb";
+import { getServerSession, User } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import { NextResponse } from "next/server";
+import UserModel from "@/models/User.model";
+import mongoose from "mongoose";
+
+export async function POST(request: Request) {
+  await connectDb();
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user;
+  if (!session || !user) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Not authenticated",
+      },
+      { status: 401 }
+    );
+  }
+  const userId = new mongoose.Types.ObjectId(user._id);
+  try {
+    const foundUser = await UserModel.aggregate([
+      { $match: { _id: userId } },
+      { $unwind: "$messages" },
+      { $sort: { "messages.createdAt": -1 } },
+      {
+        $group: {
+          _id: "$_id",
+          messages: {
+            $push: "$messages",
+          },
+        },
+      },
+    ]).exec();
+
+    if (!foundUser || foundUser.length === 0) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "User not found!",
+        },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json({
+      success: true,
+      messages: foundUser[0]?.messages,
+    });
+  } catch (err) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+}
